33dc7859a3fb2c6cc7e0cfc35c12243ff78c158a
chenjingyuan <chenjingyuan@ejiaoji.com>
2017-10-15 15:22:10 +0800

    修改代码为async await

--- Test/redisManager.js
+++ Test/redisManager.js
@@ -2,136 +2,168 @@
  * Created by frank on 16-11-3.
  */
 
-const co = require('co');
-const _ = require('lodash');
-const config = require('./config/redisConfig').redisChannel;
-const RedisManager = require('../lib/manager/RedisGlobalChannelManager');
+const _ = require("lodash");
+const config = require("./config/redisConfig").redisChannel;
+const RedisManager = require("../lib/manager/RedisGlobalChannelManager");
 
 const redisManager = new RedisManager(null, config);
 
-const serverType = 'connector';
-const serverId = ['connector_1', 'connector_2', 'connector_3'];
-const serverData = [{id: 'connector_1'}, {id: 'connector_2'}, {id: 'connector_3'}];
-const channelName = 'channelName';
+const serverType = "connector";
+const serverId = ["connector_1", "connector_2", "connector_3"];
+const serverData = [
+  { id: "connector_1" },
+  { id: "connector_2" },
+  { id: "connector_3" }
+];
+const channelName = "channelName";
 
-describe('channelName', () => {
-	before(done => {
-		co(function *() {
-			yield redisManager.start();
-			yield redisManager.clean();
-			done();
-		});
-	});
+describe("channelName", () => {
+  before(done => {
+    (async () => {
+      await redisManager.start();
+      await redisManager.clean();
+    })().then(() => {
+      done();
+    });
+  });
 
-	after(done => {
-		co(function *() {
-	        yield redisManager.stop();
-	        done();
-		});
-	});
+  after(done => {
+    (async () => {
+      await redisManager.stop();
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('add', done => {
-		co(function *() {
-			const coArr = [];
-			for (let i = 0; i < 10; i++) {
-	            coArr.push(redisManager.add(`uuid_${i}`, _.sample(serverId), channelName));
-			}
-			const result = yield coArr;
-			console.info(result);
-	        done();
-		});
-	});
+  it("add", done => {
+    (async () => {
+      const coArr = [];
+      for (let i = 0; i < 10; i++) {
+        coArr.push(
+          await redisManager.add(`uuid_${i}`, _.sample(serverId), channelName)
+        );
+      }
+      const result = await coArr;
+      console.info(result);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('getMembersBySid', done => {
-		co(function *()	{
-			const members = yield redisManager.getMembersBySid(channelName, _.sample(serverId));
-			console.info(members);
-			done();
-		});
-	});
+  it("getMembersBySid", done => {
+    (async () => {
+      const members = await redisManager.getMembersBySid(
+        channelName,
+        _.sample(serverId)
+      );
+      console.info(members);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('leave', done =>	{
-		co(function *()		{
-			const coArr = [];
-			for (const id of serverId) {
-				coArr.push(redisManager.leave('uuid_1', id, channelName));
-			}
-			const result = yield coArr;
-			console.info(result);
-			done();
-		});
-	});
+  it("leave", done => {
+    (async () => {
+      const coArr = [];
+      for (const id of serverId) {
+        coArr.push(redisManager.leave("uuid_1", id, channelName));
+      }
+      const result = await Promise.all(coArr);
+      console.info(result);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('getMembersByChannel', done => {
-		co(function *()		{
-			const members = yield redisManager.getMembersByChannelName(serverData, channelName);
-			console.info(members);
-			done();
-		});
-	});
+  it("getMembersByChannel", done => {
+    (async () => {
+      const members = await redisManager.getMembersByChannelName(
+        serverData,
+        channelName
+      );
+      console.info(members);
+    })().then(() => {
+      done();
+    });
+  });
 });
 
-describe('global service ', () => {
-	before(done =>	{
-		co(function *()	{
-			yield redisManager.start();
-			yield redisManager.clean();
-			done();
-		});
-	});
+describe("global service ", () => {
+  before(done => {
+    (async () => {
+      await redisManager.start();
+      await redisManager.clean();
+    })().then(() => {
+      done();
+    });
+  });
 
-	after(done =>	{
-		co(function *()	{
-			yield redisManager.stop();
-			done();
-		});
-	});
+  after(done => {
+    (async () => {
+      await redisManager.stop();
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('add', done =>	{
-		co(function *()		{
-			const coArr = [];
-			for (let i = 0; i < 10; i++) {
-				coArr.push(redisManager.add(`uuid_${i % 3}`, _.sample(serverId)));
-			}
-			const result = yield coArr;
-			console.info(result);
-			done();
-		});
-	});
+  it("add", done => {
+    (async () => {
+      const coArr = [];
+      for (let i = 0; i < 10; i++) {
+        coArr.push(redisManager.add(`uuid_${i % 3}`, _.sample(serverId)));
+      }
+      const result = await Promise.all(coArr);
+      console.info(result);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('getSidsByUid', done =>	{
-		co(function *()		{
-			const members = yield redisManager.getSidsByUid('uuid_1');
-			console.info(members);
-			done();
-		});
-	});
+  it("getSidsByUid", done => {
+    (async () => {
+      const members = await redisManager.getSidsByUid("uuid_1");
+      console.info(members);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('getSidsByUidArr', done =>	{
-		co(function *()		{
-			const members = yield redisManager.getSidsByUidArr(['uuid_1', 'uuid_2', 'uuid_0']);
-			console.info(members);
-			done();
-		});
-	});
+  it("getSidsByUidArr", done => {
+    (async () => {
+      const members = await redisManager.getSidsByUidArr([
+        "uuid_1",
+        "uuid_2",
+        "uuid_0"
+      ]);
+      console.info(members);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('leave', done =>	{
-		co(function *()		{
-			const coArr = [];
-			for (const id of serverId) {
-				coArr.push(redisManager.leave('uuid_1', id));
-			}
-			const result = yield coArr;
-			console.info(result);
-			done();
-		});
-	});
+  it("leave", done => {
+    (async () => {
+      const coArr = [];
+      for (const id of serverId) {
+        coArr.push(redisManager.leave("uuid_1", id));
+      }
+      const result = await Promise.all(coArr);
+      console.info(result);
+    })().then(() => {
+      done();
+    });
+  });
 
-	it('getSidsByUidArr', done =>	{
-		co(function *()		{
-			const members = yield redisManager.getSidsByUidArr(['uuid_1', 'uuid_2', 'uuid_0']);
-			console.info(members);
-			done();
-		});
-	});
+  it("getSidsByUidArr", done => {
+    (async () => {
+      const members = await redisManager.getSidsByUidArr([
+        "uuid_1",
+        "uuid_2",
+        "uuid_0"
+      ]);
+      console.info(members);
+    })().then(() => {
+      done();
+    });
+  });
 });
